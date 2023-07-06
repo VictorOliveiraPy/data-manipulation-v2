b0bbfa20b55062f02eb637cc4f23e4c0e7b4a324
Victor Hugo <victorblog410@gmail.com>
2023-07-05 23:58:13 -0300

    feature adding entity client

--- internal/entity/client.go
+++ internal/entity/client.go
@@ -2,9 +2,10 @@ package entity
 
 import (
 	"github.com/google/uuid"
+	"time"
 )
 
-type Client struct {
+type ClientRaw struct {
 	ID                 uuid.UUID
 	Document           string
 	Private            string
@@ -17,4 +18,20 @@ type Client struct {
 	Status             string
 	CreatedAt          string
 	UpdatedAt          string
+}
+
+type Client struct {
+	ID                 uuid.UUID
+	Document           string
+	DocumentType       string
+	Private            bool
+	Incomplete         bool
+	LastPurchaseDate   *time.Time
+	TicketAverage      float64
+	TicketLastPurchase float64
+	StoreMostFrequent  string
+	StoreLastPurchase  string
+	Status             string
+	CreatedAt          *time.Time
+	UpdatedAt          *time.Time
 }
